6bc145664f39f3211da6c03c77bc57a8536238dc
Maximiliano Zapata <maxzap@users.noreply.github.com>
2017-10-16 14:30:57 -0300

    otra pruba
    asdasdasdasda

--- PDO/connect.php
+++ PDO/connect.php
@@ -4,3 +4,7 @@
   $db_pass = '';
   $db = new PDO($dsn, $db_user, $db_pass);
 ?>
+
+
+select u.email, p.nombre, p.fechaCumpleano from borrowin_db.usuarioperfil as p inner join usuarios as u
+on p.objectId = u.objectId;
